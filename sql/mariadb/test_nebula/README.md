121ab2f35384454a5ca4f385e73253bb6a760e03
Szymon Derleta <szymonderleta@gmail.com>
2026-02-05 17:49:45 +0100

    Add initial SQL scripts for database schema and seed data

---
+++ sql/mariadb/test_nebula/README.md
@@ -0,0 +1,26 @@
+# Nebula Schema
+
+<div align="left">
+
+![Author](https://img.shields.io/badge/Author-Szymon%20Derleta-white?style=for-the-badge)
+
+![Release](https://img.shields.io/badge/Release-Public%20Release-green?style=for-the-badge)  
+![Version](https://img.shields.io/badge/Version-1.1.0-green?style=for-the-badge)
+
+</div>
+
+
+### Structure
+
+```filetree
+model/ – Includes the latest full database schema exported from DataGrip. A preview of the current model is shown below.
+tables/ – Contains individual SQL files generated for each database table.
+test_data/ – Stores sql files with insert queries used in test_andromeda database.
+archive/ – Stores legacy and archived materials for reference.
+```
+
+### Current Model Preview
+
+![nebula-2026-02-05.png](model/nebula-2026-02-05.png)
+
+*Nebula Schema - 2026-02-05*
---
+++ sql/mariadb/test_nebula/test_data/games.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (1, 'Chess', 1, 'https://resources.test.milkyway/public/apps//1.png', 'https://chess.test.milkyway/');
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (2, 'Element', 0, 'https://resources.test.milkyway/public/apps/2.png', 'https://element.test.milkyway/');
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (3, 'Robak', 0, 'https://resources.test.milkyway/public/apps/3.png', 'https://robak.test.milkyway/');
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (4, 'Racer', 0, 'https://resources.test.milkyway/public/apps/4.png', 'https://racer.test.milkyway/');
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (5, 'Hacman', 0, 'https://resources.test.milkyway/public/apps/5.png', 'https://hacman.test.milkyway/');
+INSERT INTO test_nebula.games (id, name, enable, icon_url, page_url) VALUES (6, 'Puzzel', 0, 'https://resources.test.milkyway/public/apps/6.png', 'https://puzzel.test.milkyway/');
